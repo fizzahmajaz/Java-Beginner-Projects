bc3c25966202dd8a665c27853fbbd16cac9e4545
Fizzahmajaz <87950179+fizzahmajaz@users.noreply.github.com>
2025-04-25 13:28:04 +0500

    mini project
    This beginner-level Java project takes a number n as input from the user and prints all even numbers from 1 to n. It demonstrates the use of:
    
    Scanner class for input handling
    
    for loop for iteration
    
    The modulus operator % to check even numbers (i % 2 == 0)
    
    Basic conditional logic using if statements
    
    This is a great practice project for mastering loops, conditions, and user input in Java.

--- evenNumber.java
+++ evenNumber.java
@@ -1,1 +1,56 @@
+import java.util.*;
+public class evenNumbers {
+
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter your value");
+        int n = sc.nextInt();
+
+
+
+        System.out.println("Even numbers till " + n + ":");
+
+        for (int i = 2; i <= n; i=i+2 ){
+
+            System.out.println(i);
+        }
+
+
+        /*
+
+        If you want to use remainder (%) Then:
+        
+        for (int i=1; i<=n; i++){
+
+             if (i % 2 == 0) {
+
+                System.out.println(i);
+          
+          }
+        } 
+          
+
+
+        OUTPUT:
+
+        Enter your value
+        17
+        Even numbers till 17:
+        2
+        4
+        6
+        8
+        10
+        12
+        14
+        16
+
+        */
+
+
+    }
+    
+}
 
